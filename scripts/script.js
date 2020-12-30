89c552530da7ceec84e6e3d22bcbb0610881b14f
Eva <renatusair@gmail.com>
2020-12-30 02:48:29 -0500

    Adding tears
    Removing audio files

--- scripts/script.js
+++ scripts/script.js
@@ -24,6 +24,7 @@ const Materials = require('Materials');
 Diagnostics.log('Console message logged from the script.');
 const Screen = require('./Screen.js');
 const mainCamera = Scene.root.findFirst('Camera');
+const tear = Scene.root.findFirst('tear');
 const Animation = require('Animation');
 const CameraInfoModule = require('CameraInfo/index.js');
 const Audio = require('Audio');
@@ -38,9 +39,12 @@ const Audio = require('Audio');
     const camera = await mainCamera;
     const face = FaceTracking.face(0);
     const feature = face.cameraTransform.applyToPoint(face.leftEye.center);
+    const rightEye = face.cameraTransform.applyToPoint(face.rightEye.center);
     var myFace = await Scene.root.findFirst('face');
-    const audioClip = await Audio.getAudioPlaybackController('audioPlaybackController');
+    const audioController = await Audio.getAudioPlaybackController('audioPlaybackController');
     const worldPosEye = await Screen.cameraTransformToFocalPlane(feature);
+    const worldPosRightEye = await Screen.cameraTransformToFocalPlane(rightEye);
+    const canvasPosRightEye = await Screen.focalPlaneToCanvas(worldPosRightEye);
     const canvasPosEye = await Screen.focalPlaneToCanvas(worldPosEye);
     const screenSize = await Screen.getFullscreenSize();
     const speaker = await Scene.root.findFirst('speaker0');
@@ -76,108 +80,75 @@ const Audio = require('Audio');
         mirror: false,
     };
 
+    const audioDriverParameters = {
+        // The duration of the driver
+        durationMilliseconds: 6000,
+
+        // The number of iterations before the driver stops
+        loopCount: 1,
+
+        // Should the driver 'yoyo' back and forth
+        mirror: false,
+    };
+
     // Create a sampler with a quadratic change in and out from -5 to 5
-    const quadraticSampler = Animation.samplers.easeInOutQuad(0.1, .43);
-    const linearSample = Animation.samplers.linear(0.1, 1);
+    const quadraticSampler = Animation.samplers.easeInOutQuad(0.1, .53);
+    const linearSample = Animation.samplers.linear(0, 1);
     /////////////////////////
     // Interactive example //
     /////////////////////////
     // const mouthOpennessDriver = Animation.valueDriver(mouthOpenness, .1, .6);
     // const scaleAnimation = Animation.animate(mouthOpennessDriver, quadraticSampler);
-    // const scaleAnimationPos = Animation.animate(mouthOpennessDriver, linearSample);
+    // const posAnimation = Animation.animate(mouthOpennessDriver, linearSample);
 
     /////////////////////////
     // Static example //
     /////////////////////////
     // Create a time driver using the parameters
     const timeDriver = Animation.timeDriver(timeDriverParameters);
+    const audioDriver = Animation.timeDriver(audioDriverParameters);
     const timeDriverLighting = Animation.timeDriver(timeDriverParameters);
     const scaleAnimation = Animation.animate(timeDriver, quadraticSampler);
-    const scaleAnimationPos = Animation.animate(timeDriver, linearSample);
-    const lightingFade = Animation.animate(timeDriverLighting, linearSample);
+    const posAnimation = Animation.animate(timeDriver, linearSample);
+    const audioFade = Animation.animate(audioDriver, linearSample);
+    const lightingFade = Animation.animate(timeDriver, linearSample);
+
+    const centerEyeTransform = worldPosEye.neg().mul(posAnimation);
 
     const myFaceTransformation = Reactive.pack3(
-        worldPosEye.x.neg().mul(scaleAnimationPos),
-        worldPosEye.y.neg().mul(scaleAnimationPos),
+        centerEyeTransform.x,
+        centerEyeTransform.y,
         scaleAnimation
     )
+    
+    const myTearTransformation = myFaceTransformation.add(worldPosRightEye).add(Reactive.pack3(0,0,0.03));
 
     myFace.transform.position = myFaceTransformation
-    // tear.transform.position = myFaceTransformation
-
+    tear.transform.position = myTearTransformation
+    tear.transform.rotation = face.cameraTransform.rotation;
+    
+    speaker.volume = audioFade;
     FaceGestures.onShake(face).subscribe(() => {
         timeDriver.start();
+        audioController.setPlaying(true);
+        audioDriver.start();
+        audioController.setLooping(true);
       });
     
     // Bind the light intensity to the intensity of the ambient light
-    var isAudioPlaying = false;
+    // var isAudioPlaying = false;
+      
+    
     timeDriver.onCompleted().subscribe(() => {
         // Start or stop the audio depending on the state of the boolean
-        audioClip.setPlaying(true);
-        speaker.volume = 1;
         ambientLight.intensity = lightingFade;
         pinkLight.intensity = lightingFade;
         pinkLight.intensity = lightingFade;
+        ambientLight.color = Reactive.RGBA(1, .9, .55, lightingFade);
 
         timeDriverLighting.start();
     })
 
-    ambientLight.intensity = Reactive.smoothStep(scaleAnimationPos.neg().add(1), .2, 1);
-    // speaker.volume = scaleAnimationPos;
-
-    // Scale face
-    // myFace.transform.scale = Reactive.pack2(SCALE, SCALE);
-
-    // var faceMat = await Materials.findFirst('material1');
-    
-    // myCanvas.material = cameraTex;
-    // myPlane.material = cameraTex;
-    // myPlane.diffuse = cameraTex;
-    // myCanvas.diffuse = cameraTex;
-
-    // (await faceMat.diffuseTextureTransform).offsetU = 1;
-    // faceMat.diffuseTextureTransform.offsetV = 10;
-    // await (faceMat.diffuseTextureTransform).offsetV(1);
-    /**
-    // Materials.get(`noise`).setTexture(tempVec4, {textureSlotName: "diffuseTexture"});
-    const face = FaceTracking.face(0);
-    const eyePos = face.cameraTransform.applyToPoint(face.leftEye.center);
-    const eyeClose = FaceTracking.face(0).rightEye.openness;
-
-    // Diagnostics.log(eyePos);
-    const pos = await Screen.cameraTransformToFocalDistance(eyePos);
-    const percentToFocalPlane = await Screen.cameraTransformToFocalPlane(eyePos);
-
-    Diagnostics.watch('x', eyePos.x);
-    Diagnostics.watch('y', eyePos.y);
-    myPlane.transform.position = await Screen.cameraTransformToFocalDistance(eyePos);
-
-
-    // myCanvas.worldTransform.position = ();
-    // Diagnostics.watch("Mouth Openness - ", eyePos);
-    // Get signal for person's eye
-    // Bind position of it 
-    // Detect when right eye close
-    // Bind signal for closed eye to transform scale & position
-
-
-// To use variables and functions across files, use export/import keyword
-// export const animationDuration = 10;
-
-// Use import keyword to import a symbol from another file
-// import { animationDuration } from './script.js'
-
-// To access scene objects
-// const [directionalLight] = await Promise.all([
-//   Scene.root.findFirst('directionalLight0')
-// ]);
-
-// To access class properties
-// const directionalLightIntensity = directionalLight.intensity;
-
-// To log messages to the console
-// Diagnostics.log('Console message logged from the script.');
-
-// Enables async/await in JS [part 2]
- **/
+    ambientLight.intensity = Reactive.smoothStep(posAnimation.neg().add(1), .2, 1);
+    // speaker.volume = posAnimation;
 })();
